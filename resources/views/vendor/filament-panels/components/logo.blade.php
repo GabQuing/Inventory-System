4524c027ee3cf30e24a4b1aaf3de8fb206f84b32
GabQuing <gab.quing29@gmail.com>
2024-11-29 22:42:47 +0800

    added a logo when dark mode

--- resources/views/vendor/filament-panels/components/logo.blade.php
+++ resources/views/vendor/filament-panels/components/logo.blade.php
@@ -1,1 +1,7 @@
-<img src="{{asset('/images/logo.png')}}" alt="logo" class="h-10">
+<div>
+    <!-- Light mode logo -->
+    <img src="{{ asset('/images/logo.png') }}" alt="Logo" class="h-10 dark:hidden">
+
+    <!-- Dark mode logo -->
+    <img src="{{ asset('/images/logo2.png') }}" alt="Logo" class="h-10 hidden dark:block">
+</div>
---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  darkMode: 'class', // Use the `class` strategy for dark mode
+  content: [
+      './resources/views/**/*.blade.php',
+      './vendor/filament/**/*.blade.php',
+  ],
+  theme: {
+      extend: {},
+  },
+  plugins: [],
+};
