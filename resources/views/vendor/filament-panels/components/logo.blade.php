744fa445221cd0b76f56a60d6330e559d38a797c
GabQuing <gab.quing29@gmail.com>
2024-12-04 23:17:48 +0800

    added change

--- resources/views/vendor/filament-panels/components/logo.blade.php
+++ resources/views/vendor/filament-panels/components/logo.blade.php
@@ -3,5 +3,5 @@
     <img src="{{ asset('/images/logo.png') }}" alt="Logo" class="h-10 dark:hidden">
 
     <!-- Dark mode logo -->
-    <img src="{{ asset('/images/logo2.png') }}" alt="Logo" class="h-10 hidden dark:block">
+    {{-- <img src="{{ asset('/images/logo2.png') }}" alt="Logo" class="h-10 hidden dark:block"> --}}
 </div>
