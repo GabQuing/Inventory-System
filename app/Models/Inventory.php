a260b877f95397f58b4230ae06b65cd7cc39ed63
GabQuing <gab.quing29@gmail.com>
2024-12-13 18:47:31 +0800

    get components items for inventory model

--- app/Models/Inventory.php
+++ app/Models/Inventory.php
@@ -4,8 +4,20 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Inventory extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'component_id',
+        'product_name' ];
+
+    public function component(): BelongsTo
+    {
+        return $this->belongsTo(Component::class);
+    }
+
+        
 }
